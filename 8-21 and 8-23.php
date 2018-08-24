c8a3b5ab76161856bdb70bd834ba21860bc67762
beast <930055912@qq.com>
2018-08-24 09:27:54 +0800

    提交修改

--- 8-21 and 8-23.php
+++ 8-21 and 8-23.php
@@ -4,7 +4,7 @@
 	2 => '已拒绝'
 	3 => '已通过'
 
-在operate_log数据库中:
+在operate_log数据库中:(我在这里)
 	提交申请
 	审核通过
 	拒绝通过
@@ -16,7 +16,7 @@ git remote rm origin
 
 //使用Auth::attempt验证用户信息
 /**
- * 1). 将传参的 password 值进行哈希加密，然后与数据库中 password 字段中已加密的密码进行匹配；
+ * 1). 将传参的 password 值进行哈希加密，然后与数据库中 password 字段中已加密的密码进行匹配；(我在这里修改)
    2). 如果匹配后两个值完全一致，会创建一个『会话』给通过认证的用户。会话在创建的同时，
  * 也会种下一个名为 laravel_session 的 HTTP Cookie，以此 Cookie 来记录用户登录状态，最终返回 true
  */
