b813cbde1d7db655d6d2d682b0481725671d0110
beast930 <34153830+beast930@users.noreply.github.com>
2018-08-25 23:12:17 +0800

    Update 8-24.php

--- 8-24.php
+++ 8-24.php
@@ -10,3 +10,36 @@ git config --global https.proxy https://127.0.0.1:34828
 //解除
 git config --global --unset http.proxy
 git config --global --unset https.proxy
+
+/********************************************************/
+从github clone的代码使用流程:
+$ composer install
+完成后，我们需要建立.env文件，因为.env默认是github所忽略的文件：
+
+$ cp .env.example .env
+因为env.example中默认没有app key，所以我们在.env中生成新的app key：
+
+$ php artisan key:generate
+接下来打开我们刚复制的.env文件，将数据库信息填入相应的位置：
+
+APP_ENV=local
+APP_KEY=base64:H6RIhyLBY-SOME-KEY-HERE-FkzCvGdS8WOU=
+APP_DEBUG=true
+APP_LOG_LEVEL=debug
+APP_URL=http://localhost
+ 
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=my_dbname
+DB_USERNAME=homestead
+DB_PASSWORD=secret
+保存后，运行
+
+$ php artisan migrate
+进行数据库迁移，如果有seeder的话，运行
+
+$ php artisan db:seed
+进行seeding即可。
+
+需要注意的是，原始项目数据库里的数据仍然需要自行拷贝。
