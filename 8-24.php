4a2bc33d64fcbfb5c0af1cb81e1ae0a0fd6126b0
beast930 <34153830+beast930@users.noreply.github.com>
2018-08-25 22:52:26 +0800

    Update 8-24.php

--- 8-24.php
+++ 8-24.php
@@ -3,3 +3,7 @@
 2.从远程仓库pull下来 提示冲突-->点击解决-->点击确定  //产生合并的冲突文件
 3.对冲突文件进行修改 commit本地
 此时 一个解决了的冲突文件已经放在本地仓库了
+
+//解决git clone 过慢
+git config --global http.https://github.com.proxy socks5://127.0.0.1:1080
+git config --global https.https://github.com.proxy socks5://127.0.0.1:1080
