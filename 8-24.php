e86ba2a6473b52403189be70399759e8ef6b595e
beast930 <34153830+beast930@users.noreply.github.com>
2018-08-25 22:54:12 +0800

    Update 8-24.php

--- 8-24.php
+++ 8-24.php
@@ -7,3 +7,6 @@
 //解决git clone 过慢
 git config --global http.https://github.com.proxy socks5://127.0.0.1:1080
 git config --global https.https://github.com.proxy socks5://127.0.0.1:1080
+//解除
+git config --global --unset http.proxy
+git config --global --unset https.proxy
