5f80b651a994091c1afddd4af2bcaf6fd0051c9f
beast930 <34153830+beast930@users.noreply.github.com>
2018-08-26 23:56:40 +0800

    Update 8-25.php

--- 8-25.php
+++ 8-25.php
@@ -1,3 +1,12 @@
 // Call the $foo->bar() method with 2 arguments
 $foo = new foo;
 call_user_func_array(array($foo, "bar"), array("three", "four"));
+
+//laravel sql语句带括号
+//use 将$ids传入闭包函数的参数, 使用where的回调函数将使sql语句包含括号
+$data = Leave::where(function ($query) use ($ids){
+            $query->whereIn('leave_id', $ids)->orWhereRaw('review_user_id = '.\Auth::user()->user_id);
+        })
+            ->whereRaw($scope->getWhere())
+            ->orderBy('created_at', 'desc')
+            ->paginate(30);
